efa6c08b957a2368b4cd5ff80cb4ebe54c797e0a
Reuben Ogbonna <reubenogbonna@gmail.com>
2019-10-21 18:56:07 -0400

    add updated answers doc with function bodies. add eslint config file

--- exercises/answers.js
+++ exercises/answers.js
@@ -1,10 +1,75 @@
+// Question 1
+function greet() {
 
-function describeAge() {
-  return 'This works.';
 }
 
+// Question 2
+function logOddNumbers() {
 
+}
+
+// Question 3
+function area() {
+
+}
+
+// Question 4
+function otherAngle() {
+
+}
+
+// Question 5
+function diameterFromArea() {
+
+}
+
+// Question 6
+function totalPrice() {
+
+}
+
+// Question 7
+function shortLongShort() {
+
+}
+
+// Question 8
+function describeAge() {
+
+}
+
+// Question 9
+function isLeapYear() {
+
+}
+
+// Question 10
+function multisum() {
+
+}
+
+// Question 11
+function asciiValue() {
+
+}
+
+// Question 12
+function stringy() {
+
+}
 
 // Do NOT touch or write anything below this line
-module.exports = { describeAge };
-// module.exports = { greet, logOddNumbers, area, otherAngle, diameterFromArea, totalPrice, shortLongShort, describeAge, isLeapYear, multisum, asciiValue, stringy };
+module.exports = {
+  greet,
+  logOddNumbers,
+  area,
+  otherAngle,
+  diameterFromArea,
+  totalPrice,
+  shortLongShort,
+  describeAge,
+  isLeapYear,
+  multisum,
+  asciiValue,
+  stringy,
+};
